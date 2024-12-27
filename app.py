95fbf7c5518ca90271421b0bba436c9a9801c2a5
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2024-12-27 22:13:48 +0530

    kotha commitlu

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from flask_mail import Mail, Message
 import random
 import os
 from authlib.jose import jwt
-from jwt import ExpiredSignatureError, InvalidTokenError
 
 app = Flask(__name__)
 CORS(app)
